cb0d4311b485d2912a695ee049821bc3d3801608
vinodkumar <vinodkumarcr059@gmail.com>
2018-10-10 10:53:38 +0530

    Project is ready to upload in heroku"

--- expense_project/settings.py
+++ expense_project/settings.py
@@ -117,8 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 STATICFILES_DIRS=[
             os.path.join(BASE_DIR, 'money/static'),
 ]
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
